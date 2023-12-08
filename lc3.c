5ab14e610d4a95a583eb145e22c6c9a2c0097f9a
Semen <pe1gno1res@mail.ru>
2023-12-08 18:02:20 +0300

    Final

--- lc3.c
+++ lc3.c
@@ -1,10 +1,26 @@
-// Memory Storage
-#define MEMORY_MAX (1 << 16)
 
-#include <stdio.h>
-#include <stdint.h>
-
-uint16_t memory[MEMORY_MAX]; /* 65536 locations*/
+#ifdef LINUX
+    // unix includes
+    #include <stdio.h>
+    #include <stdint.h>
+    #include <signal.h>
+    /* unix only*/
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <sys/time.h>
+    #include <sys/types.h>
+    #include <sys/termios.h>
+    #include <sys/mman.h>
+#elif _WIN32 || _WIN64
+    // Windows includes
+    #include <stdio.h>
+    #include <stdint.h>
+    #include <signal.h>
+    /* windows only */
+    #include <Windows.h>
+    #include <conio.h>  // _kbhit
+#endif
 
 // Registers
 enum 
@@ -22,8 +38,7 @@ enum
     R_COUNT
 };
 
-// Register Storage
-uint16_t reg[R_COUNT];
+
 
 // Condition Flags
 enum
@@ -54,6 +69,13 @@ enum
     OP_TRAP     /* execute trap*/
 };
 
+//Memory Mapped Registers
+enum
+{
+    MR_KBSR = 0xFE00, /* keyboard status */
+    MR_KBDR = 0xFE02  /* keyboard data */
+};
+
 //TRAP codes
 enum
 {
@@ -65,6 +87,79 @@ enum
     TRAP_HALT = 0x25   /* halt the program */
 };
 
+// Memory Storage
+#define MEMORY_MAX (1 << 16)
+
+// Register Storage
+uint16_t reg[R_COUNT];
+uint16_t memory[MEMORY_MAX]; /* 65536 locations*/
+
+// INput Buffering
+#ifdef LINUX
+    // Input Buffering linux
+    struct termios original_tio;
+
+    void disable_input_buffering()
+    {
+        tcgetattr(STDIN_FILENO, &original_tio);
+        struct termios new_tio = original_tio;
+        new_tio.c_lflag &= ~ICANON & ~ECHO;
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+    }
+
+    void restore_input_buffering()
+    {
+        tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+    }
+
+    uint16_t check_key()
+    {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 0;
+        return select(1, &readfds, NULL, NULL, &timeout) != 0;
+    }
+#elif _WIN32 || _WIN64
+    // Input Buffering Windows
+    HANDLE hStdin = INVALID_HANDLE_VALUE;
+    DWORD fdwMode, fdwOldMode;
+
+    void disable_input_buffering()
+    {
+        hStdin = GetStdHandle(STD_INPUT_HANDLE);
+        GetConsoleMode(hStdin, &fdwOldMode); /* save old mode */
+        fdwMode = fdwOldMode
+                ^ ENABLE_ECHO_INPUT  /* no input echo */
+                ^ ENABLE_LINE_INPUT; /* return when one or
+                                        more characters are available */
+        SetConsoleMode(hStdin, fdwMode); /* set new mode */
+        FlushConsoleInputBuffer(hStdin); /* clear buffer */
+    }
+
+    void restore_input_buffering()
+    {
+        SetConsoleMode(hStdin, fdwOldMode);
+    }
+
+    uint16_t check_key()
+    {
+        return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+    }
+#endif
+
+// Handle Interrupt
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+// Sign Extend
 uint16_t sign_extend(uint16_t x, int bit_count)
 {
     if((x >> (bit_count - 1)) & 1)
@@ -75,6 +170,13 @@ uint16_t sign_extend(uint16_t x, int bit_count)
     return x;
 }
 
+// Swap
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+// Update Flags
 void update_flags(uint16_t r)
 {
     if(reg[r] == 0)
@@ -92,11 +194,61 @@ void update_flags(uint16_t r)
     }
 }
 
+//Read Image File
 void read_image_file(FILE * file)
 {
-    
+    /* the origin tells us where in memory to place the image */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1 , file);
+    origin = swap16(origin);
+
+    /* we know maximum file size so we only need one fread */
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t * p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    /* swap to little endian */
+    while(read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
 }
 
+// Read Image
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return 0; };
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+// Memory Access
+void mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
+// Main Loop
 int main(int argc, const char * argv[])
 {
     //Load Arguments
@@ -117,7 +269,8 @@ int main(int argc, const char * argv[])
     }
 
     // Setup
-
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 /* since exactly one condition flag should be set at any
  given time, set the Z flag */
 
@@ -354,4 +507,5 @@ int main(int argc, const char * argv[])
         }   
     }
     //@{Shutdown}
+    restore_input_buffering();
 }
